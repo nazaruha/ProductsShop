873657454c824f86aeee96f1be1001b73c23ca2e
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 17:08:31 +0300

    Add script 'tblProducts'

---
+++ DB_Worker/SqlTables/tblProducts.sql
@@ -0,0 +1,16 @@
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblProducts]'))
+EXEC dbo.sp_executesql @statement = N'
+CREATE TABLE [dbo].[tblProducts](
+  [Id] [int] IDENTITY PRIMARY KEY NOT NULL,
+  [Name] [nvarchar](100) NOT NULL,
+  [Count] [int],
+  [Price] [int] NOT NULL,
+  [CategoryId] [int] NOT NULL,
+  [SubCategoryId] [int] NOT NULL,
+
+  COSNTRAINT [FK_tblProducts_tblCategories] FOREIGN KEY([CategoryId])
+   REFERENCES [dbo].[tblCategories] ([Id]),
+
+  CONSTRAINT [FK_tblProducts_tblSubCategories] FOREIGN KEY([SubCategoryId])
+   REFERENCES [dbo].[tblSubCategories] ([Id])
+);'
