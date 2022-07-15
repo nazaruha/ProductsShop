726423b4e490ac163eb29b64961d9d4512a2fc8c
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 16:56:14 +0300

    add creating database

--- DB_Worker/Program.cs
+++ DB_Worker/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace DB_Worker
 {
-    internal class Program
+    class Program
     {
+        static string database = "ProductsShop2";
+        static string dirSql = "SqlTables";
+        static string dirScripts = "Scripts";
+        static SqlConnection con;
+        static SqlCommand cmd;
+
         static void Main(string[] args)
         {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string connectionDB = "Data Source=.;Integrated Security=True;";
+            con = new SqlConnection(connectionDB);
+            con.Open();
+            if (!IsDBExists())
+            {
+                cmd.CommandText = $"CREATE DATABASE {database}";
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("DB is created"); ;
+            }
+            con = new SqlConnection(connectionDB + $"Initial Catalog={database}");
+            con.Open();
+            cmd = con.CreateCommand();
+            Console.WriteLine("END");
+        }
+
+        static bool IsDBExists()
+        {
+            cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT name FROM master.sys.databases";
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    if (reader["name"].ToString() == database)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
