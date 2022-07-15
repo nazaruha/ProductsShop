19f339ef1e3ed89992f34cb135a6482f9c4b5a5b
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 17:13:36 +0300

    Add script 'tblSales'

---
+++ DB_Worker/SqlTables/tblSales.sql
@@ -0,0 +1,13 @@
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblSales]'))
+EXEC dbo.sp_executesql @statement = N'
+CREATE TABLE [dbo].[tblSales](
+  [ClientId] [int] NOT NULL,
+  [ProductId] [int] NOT NULL,
+  [Count] [int] NOT NULL,
+
+  COSNTRAINT [FK_tblBasket_tblClients] FOREIGN KEY([ClientId])
+   REFERENCES [dbo].[tblClients] ([Id]),
+
+  COSNTRAINT [FK_tblBasket_tblProducts] FOREIGN KEY([ProductId])
+   REFERENCES [dbo].[tblProducts] ([Id])
+);'
