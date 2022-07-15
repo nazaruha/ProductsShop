1c3f28c772d10272a20747ff6a5af6a0f7f8762a
Nazar <fedunnazar20122001@gmail.com>
2022-07-15 17:02:51 +0300

    add script 'tblSubCategories'

---
+++ DB_Worker/SqlTables/tblSubCategories.sql
@@ -0,0 +1,10 @@
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblSubCategories]'))
+EXEC dbo.sp_executesql @statement = N'
+CREATE TABLE [dbo].[tblSubCategories](
+  [Id] [int] IDENTITY PRIMARY KEY NOT NULL,
+  [CategoryId] [int] NOT NULL,
+  [Name] [nvarchar](100) NOT NULL,
+
+  CONTRAINT [FK_tblSubCategories_tblCategories] FOREIGN KEY([CategoryId])
+   REFERENCES [dbo].[tblCategories] ([Id])
+);'
