e07050c4f35ee04d23a792d45cd7c582acb0a48b
Nazar <fedunnazar20122001@gmail.com>
2022-07-18 16:10:44 +0300

    class for dataGrid in Basket

---
+++ EXAM_ProductShop/Models/ClientBasketProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM_ProductShop.Models
+{
+    public class ClientBasketProduct
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+
+        public ClientBasketProduct()
+        {
+
+        }
+    }
+}
